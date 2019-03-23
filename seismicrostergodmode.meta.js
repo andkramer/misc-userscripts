71561c0fc262b8c77a829e340bb0844478c417e7
Andre Kramer <and.kramer@live.de>
2019-03-23 20:01:45 +0100

    Update seismicrostergodmode.meta.js

--- seismicrostergodmode.meta.js
+++ seismicrostergodmode.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Seismic Roster Godmode
-// @version      0.0.3
+// @version      0.1.0
 // @license      GPL-3.0
 // @namespace    https://github.com/andkramer
 // @run-at       document-idle
 // @description  TODO
 // @author       Andre Kramer (and.kramer@live.de)
 // @match        https://seismicgaming.eu/divisions/*/roster
-// @grant        none
+// @grant        GM.setValue
+// @grant        GM.getValue
 // @updateURL    https://raw.githubusercontent.com/andkramer/misc-userscripts/master/seismicrostergodmode.meta.js
 // @downloadURL  https://raw.githubusercontent.com/andkramer/misc-userscripts/master/seismicrostergodmode.user.js
 // ==/UserScript==
