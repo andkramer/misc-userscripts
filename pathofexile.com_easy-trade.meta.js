1c1d14873283ce8dea672b0c644066df7f1323ae
Andre Kramer <15822856+andkramer@users.noreply.github.com>
2026-08-04 07:46:36 +0200

    Update pathofexile.com_easy-trade.meta.js

--- pathofexile.com_easy-trade.meta.js
+++ pathofexile.com_easy-trade.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PoE Trade Quick Filters
 // @namespace    poe-trade-qf
-// @version      4.0
+// @version      4.1
 // @description  Compact mirror bar for the PoE trade search filters
 // @match        https://www.pathofexile.com/trade/search/*
 // @grant        none
