59ab3855cdf0601e9c1901ca4de18e146018f9cd
Andre Kramer <15822856+andkramer@users.noreply.github.com>
2026-08-04 07:28:08 +0200

    Create pathofexile.com_easy-trade.meta.js

---
+++ pathofexile.com_easy-trade.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         PoE Trade Quick Filters
+// @namespace    poe-trade-qf
+// @version      4.0
+// @description  Compact mirror bar for the PoE trade search filters
+// @match        https://www.pathofexile.com/trade/search/*
+// @grant        none
+// @run-at       document-idle
+// @updateURL    https://github.com/andkramer/misc-userscripts/raw/refs/heads/master/pathofexile.com_easy-trade.meta.js
+// @downloadURL  https://github.com/andkramer/misc-userscripts/raw/refs/heads/master/pathofexile.com_easy-trade.user.js
+// ==/UserScript==
