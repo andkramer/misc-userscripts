8c4cb0e216cc716f6d135b40d548c6e441f64ef0
Andre Kramer <15822856+andkramer@users.noreply.github.com>
2026-08-04 07:46:24 +0200

    Update pathofexile.com_easy-trade.user.js

--- pathofexile.com_easy-trade.user.js
+++ pathofexile.com_easy-trade.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PoE Trade Quick Filters
 // @namespace    poe-trade-qf
-// @version      4.0
+// @version      4.1
 // @description  Compact mirror bar for the PoE trade search filters
 // @match        https://www.pathofexile.com/trade/search/*
 // @grant        none
@@ -45,7 +45,7 @@
   const RANGE_FILTERS = [
     { label: 'iLvl',    filterTitle: 'Item Level' },
     { label: 'Gem Lvl', filterTitle: 'Gem Level' },
-    { label: 'Qual',    filterTitle: 'Quality' }
+    { label: 'Quality',    filterTitle: 'Quality' }
   ];
 
   const DROPDOWN_FILTERS = [
